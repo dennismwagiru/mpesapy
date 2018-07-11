6aca0a309d46a5e50fed1eab6577492dc48b67f4
Dennis Mwagiru <dennismwagiru@gmail.com>
2018-07-11 19:48:08 +0300

    Working b2c_payment

--- mpesapy/__init__.py
+++ mpesapy/__init__.py
@@ -21,11 +21,24 @@ class Mpesa:
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
 
+    def b2c_payment_request(self, **kwargs):
+        expected_keys = ['InitiatorName', 'SecurityCredential', 'CommandID', 'Amount', 'PartyA', 'PartyB', 'Remarks',
+                         'QueueTimeOutURL', 'ResultURL', 'Occassion']
+        payload = self.process_kwargs(expected_keys, kwargs)
+        url = URL['sandbox']['b2c_payment_request']
+        access_token = self.get_access_token()
+        headers = {
+            'Authorization': 'Bearer ' + access_token
+        }
+        res = requests.post(url=url, json=payload, headers=headers).text
+        return json.loads(res)
+
+
     def c2b_simulate(self, **kwargs):
         expected_keys = ['CommandID', 'Amount', 'Msisdn', 'BillRefNumber']
         payload = self.process_kwargs(expected_keys, kwargs)
         payload['ShortCode'] = self.short_code
-        url = URL['sandbox']['c2bsimulate']
+        url = URL['sandbox']['c2b_simulate']
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -39,7 +52,7 @@ class Mpesa:
         payload = self.process_kwargs(expected_keys, kwargs)
         payload['ResponseType'] = 'Completed'
         payload['ShortCode'] = self.short_code
-        url = URL['sandbox']['registerurl']
+        url = URL['sandbox']['register_url']
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
--- mpesapy/urls.py
+++ mpesapy/urls.py
@@ -5,5 +5,6 @@ URL = {}
 URL['sandbox'] = {}
 URL['sandbox']['authentication'] = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 URL['sandbox']['online_checkout'] = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-URL['sandbox']['registerurl'] = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-URL['sandbox']['c2bsimulate'] = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+URL['sandbox']['register_url'] = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+URL['sandbox']['c2b_simulate'] = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+URL['sandbox']['b2c_payment_request'] = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
