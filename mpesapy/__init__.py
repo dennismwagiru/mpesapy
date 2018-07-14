02001e58bf30f27114463ae51cc45679d7ff01e6
Dennis Mwagiru <dennismwagiru@gmail.com>
2018-07-14 12:54:38 +0300

    Transaction reversal, revamp urls

--- mpesapy/__init__.py
+++ mpesapy/__init__.py
@@ -3,8 +3,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 import base64
 
-from .urls import URL
-
 """
 mpesapy
 
@@ -21,11 +19,23 @@ class Mpesa:
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
 
+    def reversal(self, **kwargs):
+        expected_keys = ['CommandID', 'ReceiverParty', 'ReceiverIdentifierType', 'Remarks', 'Initiator',
+                         'SecurityCredential', 'QueueTimeOutURL', 'ResultURL', 'TransactionID', 'Occasion']
+        payload = self.process_kwargs(expected_keys, kwargs)
+        url = self.get_url('reversal')
+        access_token = self.get_access_token()
+        headers = {
+            'Authorization': 'Bearer ' + access_token
+        }
+        res = requests.post(url=url, json=payload, headers=headers).text
+        return json.loads(res)
+
     def transaction_status(self, **kwargs):
         expected_keys = ['CommandID', 'PartyA', 'IdentifierType', 'Remarks', 'Initiator', 'SecurityCredential',
                          'QueueTimeOutURL', 'ResultURL', 'TransactionID', 'Occasion']
         payload = self.process_kwargs(expected_keys, kwargs)
-        url = URL['sandbox']['transaction_status']
+        url = self.get_url('transaction_status')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -37,7 +47,7 @@ class Mpesa:
         expected_keys = ['CommandID', 'PartyA', 'IdentifierType', 'Remarks', 'Initiator', 'SecurityCredential',
                          'QueueTimeOutURL', 'ResultURL']
         payload = self.process_kwargs(expected_keys, kwargs)
-        url = URL['sandbox']['balance']
+        url = self.get_url('account_balance')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -50,7 +60,7 @@ class Mpesa:
                          'Remarks', 'Initiator', 'SecurityCredential', 'QueueTimeOutURL', 'ResultURL',
                          'AccountReference']
         payload = self.process_kwargs(expected_keys, kwargs)
-        url = URL['sandbox']['b2b_payment_request']
+        url = self.get_url('b2b_payment_request')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -62,7 +72,7 @@ class Mpesa:
         expected_keys = ['InitiatorName', 'SecurityCredential', 'CommandID', 'Amount', 'PartyA', 'PartyB', 'Remarks',
                          'QueueTimeOutURL', 'ResultURL', 'Occassion']
         payload = self.process_kwargs(expected_keys, kwargs)
-        url = URL['sandbox']['b2c_payment_request']
+        url = self.get_url('b2c_payment_request')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -75,7 +85,7 @@ class Mpesa:
         expected_keys = ['CommandID', 'Amount', 'Msisdn', 'BillRefNumber']
         payload = self.process_kwargs(expected_keys, kwargs)
         payload['ShortCode'] = self.short_code
-        url = URL['sandbox']['c2b_simulate']
+        url = self.get_url('c2b_simulate')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -89,7 +99,7 @@ class Mpesa:
         payload = self.process_kwargs(expected_keys, kwargs)
         payload['ResponseType'] = 'Completed'
         payload['ShortCode'] = self.short_code
-        url = URL['sandbox']['register_url']
+        url = self.get_url('register_url')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -106,7 +116,7 @@ class Mpesa:
         payload['Password'] = key
         payload['BusinessShortCode'] = self.short_code
         payload['TransactionType'] = 'CustomerPayBillOnline'
-        url = URL[self.env]['online_checkout']
+        url = self.get_url('online_checkout')
         access_token = self.get_access_token()
         headers = {
             'Authorization': 'Bearer ' + access_token
@@ -128,7 +138,7 @@ class Mpesa:
         return payload
 
     def get_access_token(self):
-        url = URL[self.env]['authentication']
+        url = self.get_url('authentication')
         res = requests.get(url,params=dict(grant_type='client_credentials'),
                            auth=requests.auth.HTTPBasicAuth(self.consumer_key, self.consumer_secret)).text
         res_json = json.loads(res)
@@ -136,3 +146,20 @@ class Mpesa:
         if 'errorMessage' in res_json:
             raise ValueError(res_json['errorMessage'])
         return res_json['access_token']
+
+    def get_url(self, api):
+        urls = {
+            'sandbox' : {
+                'authentication': 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+                'online_checkout': 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
+                'register_url': 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl',
+                'c2b_simulate': 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate',
+                'b2c_payment_request': 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest',
+                'b2b_payment_request': 'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest',
+                'account_balance': 'https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query',
+                'transaction_status': 'https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query',
+                'reversal': 'https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request'
+            }
+        }
+
+        return urls[self.env][api]
